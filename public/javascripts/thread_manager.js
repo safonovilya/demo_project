38e393b844587362df1be0fe2d6013568cf68dc5
Ilya Safonov <safonov.ilya@gmail.com>
2012-07-11 17:26:23 +0600

    fix bugs

--- public/javascripts/thread_manager.js
+++ public/javascripts/thread_manager.js
@@ -2,13 +2,18 @@ $(document).ready(function(){
     var editor = $("#editor");
     var comments = $("#comments");
     $("#send").click(function(){
+        if (comments.find(".thread").length > 0 && editor.find('input#rootID').length == 0){
+            alert("enter the thread!");
+            return false;
+        }
         $.post(
             "http://localhost:3000/add/",
             editor.serialize(),
             function(data){
                 var ul = $(".thread[data-id="+editor.find('input#rootID').val()+"]>ul");
+                console.log(ul);
                 if (ul.length == 1){
-                    ul.append($("<li>",{text: data}));
+                    ul.append($("<li>",{html: data}));
                 } else {
                     console.log(data);
                     comments.append(data);
